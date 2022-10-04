0230908a97fa70d58c6eb6f338100f01e549a126
AhmetKucukaydiner <ahmetkucukaydiner@gmail.com>
2022-10-04 23:51:50 +0300

    Add project files.

---
+++ tersten_karakter_yazdirma/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace tersten_karakter_yazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir değer giriniz: ");
+            string sentence = Console.ReadLine();
+            CharReverse(sentence);
+        }
+
+        static void CharReverse(string sentence)
+        {
+            string newSentence = "";  
+            string letter = "";
+
+            for (int i = 0; i <= sentence.Length; i++)
+            {
+                if (i == 0)
+                {
+                    letter += sentence[i].ToString(); 
+                }
+                else if (i == sentence.Length)
+                {
+                    newSentence += letter; 
+                }
+                else if (sentence[i - 1].ToString() == " ")
+                    continue;
+                else if (sentence[i].ToString() == " ")
+                {
+                    newSentence += letter + " ";  
+                    letter = sentence[i + 1].ToString();
+                }
+                else
+                    newSentence += sentence[i].ToString();
+            }
+            Console.WriteLine(newSentence);
+        }
+    }
+}
